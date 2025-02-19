3845bde0b09f8f32069d1f6cc863c2a003de6d3c
Shubham-Gattani <21f3002082@ds.study.iitm.ac.in>
2025-02-19 12:55:44 +0530

    Wrote a code to get all the data (raw and normalized), and create visualizations for home page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    visualizations_dir = os.path.join(app.static_folder, 'visualizations')
+    images = [f for f in os.listdir(visualizations_dir) if f.endswith('.png')]    
+    return render_template('index.html', images=images)
 
 if __name__ == '__main__':
     app.run(debug=True)
