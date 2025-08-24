5c688c68b0a4e56eb92f5ab83606349721a29c9f
Shubham-Gattani <21f3002082@ds.study.iitm.ac.in>
2025-08-24 12:09:10 +0530

    Removed data of ES courses

--- subject_wise_engagement/fetch_category_IDs_107.py
+++ subject_wise_engagement/fetch_category_IDs_107.py
@@ -5,36 +5,15 @@ import pandas as pd
 import time, os
 import json  # Importing json to handle JSON data
 from functools import lru_cache 
-from subject_wise_engagement.execute_query import execute_query_107
-# from execute_query import execute_query_107
+# from subject_wise_engagement.execute_query import execute_query_107
+from execute_query import execute_query_107
 
 
 start = time.time()
 df_map_category_to_id = execute_query_107(query_id=107,query_params=None)
-
-irrelevant_categories = """Course LxIs
-Math for Electronics I
-Electronic Systems Thinking and Circuits
-Basic Digital Systems
-Electrical and Electronic Circuits
-Embedded C Programming
-Math for Electronics II
-Signals and Systems
-Analog Electronic Systems
-Digital Signal Processing
-Control Engineering
-Digital System Design
-Sensors and Application
-Electronics Lab
-Introduction to Linux Shell
-electromagnetic_fields_and_transmission_lines
-electronic_product_design"""
-irrelevant_categories = irrelevant_categories.replace("\n",",").split(",")
-# Remove rows where "name" column doesn't have any of the irrelevant categories
-df_map_category_to_id = df_map_category_to_id[~df_map_category_to_id["name"].isin(irrelevant_categories)]
+irrelevant_categories = [63,64,79,80,86,87,88,91,95,96,97,103,104,105,106,107,112,113,114]
+df_map_category_to_id = df_map_category_to_id[~df_map_category_to_id["category_id"].isin(irrelevant_categories)]
+df_map_category_to_id.to_csv("category_id_name_mapping.csv", index=False)
 end = time.time()
-# print(f"Time taken to fetch category IDs: {round(end - start, 2)} seconds")
-# with open("time_log.txt", "a") as f:
-#     f.write(f"\n\n***********Time taken to df_map_category_to_id\n\n****************\n\n: {round(end - start, 2)} seconds\n")
 if __name__ == "__main__":
     print(df_map_category_to_id.head())
