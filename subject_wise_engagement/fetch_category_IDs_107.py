385e178c4cccf6bb7578d04a4c68a30613d76dba
Shubham-Gattani <21f3002082@ds.study.iitm.ac.in>
2025-08-24 15:58:00 +0530

    fixed a bug at line 8

--- subject_wise_engagement/fetch_category_IDs_107.py
+++ subject_wise_engagement/fetch_category_IDs_107.py
@@ -5,8 +5,8 @@ import pandas as pd
 import time, os
 import json  # Importing json to handle JSON data
 from functools import lru_cache 
-# from subject_wise_engagement.execute_query import execute_query_107
-from execute_query import execute_query_107
+from subject_wise_engagement.execute_query import execute_query_107
+# from execute_query import execute_query_107
 
 
 start = time.time()
